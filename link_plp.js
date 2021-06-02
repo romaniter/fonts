79d3c96a15d4fba50dedbe7a953df6505da055b0
Roman <rkproger@gmail.com>
2021-06-02 13:31:08 +0300

    Update link_plp.js

--- link_plp.js
+++ link_plp.js
@@ -1,10 +1,11 @@
 (function() {
     'use strict';
-
+try {
     if (plp_page_id) {
         var url = '<a href="https://app.platformalp.ru/#!/redactor/'+ plp_page_id + '?content=' + plp_content_id + '" target="_blank" style="position:fixed;top:0px;right:0px;z-index:999999999;"> Перейти в редактор </a>';
         var div = document.createElement("div");
         div.innerHTML = url;
         document.body.appendChild(div);
     }
+} catch {}
 })();
