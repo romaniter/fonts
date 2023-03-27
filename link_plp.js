d66fe98aa0526a70c3f736a07a95fbef3d4c6d4d
Roman <rkproger@gmail.com>
2023-03-27 10:07:27 +0300

    add 3.7 support

--- link_plp.js
+++ link_plp.js
@@ -2,10 +2,17 @@
     'use strict';
 try {
     if (plp_page_id) {
-        var url = '<a href="https://app.platformalp.ru/#!/redactor/'+ plp_page_id + '?content=' + plp_content_id + '" target="_blank" style="position:fixed;top:0px;right:0px;z-index:999999999;"> Перейти в редактор </a>';
+        if (plp.VERSION == '3.7') {
+            var url = '<a href="https://new.platformalp.ru/editor/';
+        } else {
+            var url = '<a href="https://app.platformalp.ru/#!/redactor/';
+        }
+        url = url + plp_page_id + '?content=' + plp_content_id + '" target="_blank" style="position:fixed;top:0px;right:0px;z-index:999999999;"> Перейти в редактор </a>';
         var div = document.createElement("div");
         div.innerHTML = url;
         document.body.appendChild(div);
     }
-} catch {}
+} catch (error) {
+    console.log(error);
+}
 })();
